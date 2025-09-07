972efc6772e41e07a87e9a0a3d6143fe8f558e14
ravali-99 <ravali9498@gmail.com>
2025-09-07 17:34:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0360c520857e3138f"
   instance_type          = "t2.medium"
-  key_name               = "terraformkeypair"
-  vpc_security_group_ids = ["sg-0f1d5f2f18bb4af01"]
+  key_name               = "rudrash"
+  vpc_security_group_ids = ["sg-07b94e14273060c3e"]
   tags = {
     Name = var.instance_names[count.index]
   }
